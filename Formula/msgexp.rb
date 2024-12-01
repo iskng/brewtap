56f91d5f202953e1bee5e95e79de3f690b5f7365
lynxeco <kenji@lynx.eco>
2024-12-01 12:04:13 -0800

    version bump

--- Formula/msgexp.rb
+++ Formula/msgexp.rb
@@ -1,7 +1,7 @@
 class Msgexp < Formula
   desc "Tool to export iMessage data and run iMessage diagnostics"
   homepage "https://github.com/iskng/imessage-exporter"
-  version "2.1.1c"
+  version "2.1.1d"
 
   url "https://github.com/iskng/imessage-exporter/releases/download/#{version}/imessage-exporter"
   sha256 "836cceca5573f005dafb8f859b74e35c22869a5372935599c786eb4af3fab5c1"
