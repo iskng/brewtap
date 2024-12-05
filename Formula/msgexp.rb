f18fb8127a020d3e53448890c60676930a52c9ff
lynxeco <kenji@lynx.eco>
2024-12-04 21:13:06 -0800

    bumb version and sha

--- Formula/msgexp.rb
+++ Formula/msgexp.rb
@@ -1,10 +1,10 @@
 class Msgexp < Formula
   desc "Tool to export iMessage data and run iMessage diagnostics"
   homepage "https://github.com/iskng/imessage-exporter"
-  version "2.1.1f"
+  version "2.1.1g"
 
   url "https://github.com/iskng/imessage-exporter/releases/download/#{version}/imessage-exporter"
-  sha256 "61a3fe6ed2639f2fad23a9dcc71b53f243b3d6bc647f8a468ea1b6e488aac6a9"
+  sha256 "0ba6b4a372595d19a8b50dc5792746a187e5472a541c0092a850959b716b8b9a"
 
   depends_on :macos
   depends_on arch: :arm64
